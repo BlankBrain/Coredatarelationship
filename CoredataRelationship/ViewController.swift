3501a7b2c3e32005ed6daa3d938416812bc37402
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-28 16:22:06 +0600

    Initial Commit

---
+++ CoredataRelationship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoredataRelationship
+//
+//  Created by Md. Mehedi Hasan on 28/1/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
